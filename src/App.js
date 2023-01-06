b8fb7167772e6e974bc852f7e00ca0b27264aec0
Dimes1991 <dimss.dev@gmail.com>
2023-01-07 00:06:23 +0800

    can save note

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import NoteCard from './components/NoteCard';
+import NoteForm from './components/NoteForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <NoteForm />
+      <NoteCard title='example' body='ini body' color='gray'/>
+    
     </div>
   );
 }
---
+++ src/components/NoteForm/NoteForm.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import "./NoteForm.style.css";
+
+const INITIAL_STATES = {
+  formData: {
+    title: "",
+    body: "",
+    color: "white",
+  },
+};
+
+function NoteForm() {
+  // const [getter <any>, setter <function>]
+  const [formData, setFormData] = useState({ ...INITIAL_STATES.formData });
+
+  /**
+   * @description - Changing title
+   * @param {string} value
+   */
+  const onTitleChange = (value = "") => {
+    setFormData({
+      ...formData,
+      title: value,
+    });
+  };
+
+  /**
+   * @description - Change body
+   * @param {string} value
+   */
+  const onBodyChange = (value = "") => {
+    setFormData({
+        ...formData,
+        body: value,
+    });
+  };
+
+  /**
+   * @description - Sets color of note if the color item is clicked
+   * @param {string} selectedColor - color of the selected item color
+   */
+  const onColorItemClick = (selectedColor = "") => {
+    setFormData({ ...formData, color: selectedColor });
+  };
+  /**
+   * @description - on form submit, create new note
+   * @param {Event} evt
+   */
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+
+    //Validation
+    const isTitleValid = !!formData.title;
+    if (!isTitleValid) {
+      alert("Setidaknya tulis title dek!");
+      return;
+    }
+
+    //Saving
+    localStorage.setItem("catatan", JSON.stringify(formData));
+
+    //Clean form
+    setFormData({ ...INITIAL_STATES.formData });
+  };
+
+  return (
+    <div className="c-note-form" style={{ backgroundColor: formData.color }}>
+      <form name="note-form" autoComplete="off" onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="note-title"
+          id="note-title"
+          placeholder="title"
+          className="note-title-input"
+          defaultValue={formData.title}
+          onChange={(evt) => onTitleChange(evt.target.value)}
+        />
+
+        <textarea
+          name="note-body"
+          id="note-body"
+          cols="30"
+          rows="10"
+          placeholder="write..."
+          className="note-body-input"
+          defaultValue={formData.body}
+          onChange={(evt) => onBodyChange(evt.target.value)}
+        ></textarea>
+
+        <div className="c-color-selector">
+          <div
+            className={`c-color-selector__item c-color-selector__item--white ${
+              formData.color === "white"
+                ? "c-color-selector__item--selected"
+                : ""
+            }`}
+            onClick={() => onColorItemClick("white")}
+          ></div>
+          <div
+            className={`c-color-selector__item c-color-selector__item--pink ${
+              formData.color === "pink"
+                ? "c-color-selector__item--selected"
+                : ""
+            }`}
+            onClick={() => onColorItemClick("pink")}
+          ></div>
+          <div
+            className={`c-color-selector__item c-color-selector__item--grey ${
+              formData.color === "grey"
+                ? "c-color-selector__item--selected"
+                : ""
+            }`}
+            onClick={() => onColorItemClick("grey")}
+          ></div>
+        </div>
+
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+}
+
+export default NoteForm;
